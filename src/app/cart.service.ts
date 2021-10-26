8a6a15b174dda7d73f8a563abab97fa24d490bc6
jtpunt <jonathan.perry1994@gmail.com>
2021-10-26 16:34:26 -0600

    added cart service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable()
+export class CartService {
+  // Define an items property to store the array of the current products in the cart
+  items: Product[] = [];
+  // This method appends a producct to an array of items
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+  // This method collects the items users add to the cart and returns each item with its associated quantity
+  getItems() {
+    return this.items;
+  }
+  // This method returns an empty array of items, which empties the cart
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  constructor() {}
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 // Import the Product class and products array from ../products
 import { Product, products } from '../products';
-
+// Import the cart service
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -14,12 +15,23 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   // Define the product property
   product: Product | undefined;
+  // This method does the following:
+  // - Takes the current product as an argument
+  // - Uses the CartService addToCart() method to add the product to the cart
+  // - Displays a message that you've added a product to the cart
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
   // Inject ActivatedRoute into the constructor() by adding 'private route: ActivatedRoute'
   // as an argument within the constructor's parentheses.
   // ActivatedRoute is specific to each component that the Angular Router loads. ActivatedRoute
   // contains information about the route and the route's parameters. By injecting ActivatedRoute.
   // you are configuring the component to use a service.
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   // In the ngOnInit() method, extract the productId from the route parameters and find the corresponding product in the products array
   ngOnInit() {
     // First get the product id from the current route.
